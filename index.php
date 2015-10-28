d045a94596d7554058e775c7bdca59dc7b694fe2
nitish24p <nitish.phanse@practo.com>
2015-10-29 01:42:41 +0530

    crawler basic functionality added in index file

--- index.php
+++ index.php
@@ -1,23 +1,75 @@
 <?php 
 
 $to_crawl = "";
+$c = array();
 
 function get_links($url){
+	global $c;
 	$input = @file_get_contents($url);
 	$regex = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
 	preg_match_all("/$regex/siU", $input, $matches);
-	
+	$base_url = parse_url($url , PHP_URL_HOST);
 	$l = $matches[2];
 	foreach ($l as $link) {
+		// for SPA URLs
 		if (strpos($link, "#")) {
 			$links = substr($link, 0 , strpos($link, "#"));
 		}
+		// check if URLs start with a .
 		if (substr($link , 0 , 1) == ".") {
 			$link = substr($link, 1);
 		}
-		echo $link."</br>";
+		// check if http:// or https:// or mail id 
+		if (substr($link, 0 , 7) == "http://") {
+			$link = $link;
+		}
+		else if (substr($link, 0 , 8) == "https://") {
+			$link = $link;
+		}
+		else if (substr($link, 0 , 2) == "//") {
+			$link = substr($link, 2);
+		}
+		else if (substr($link, 0 , 1) == "#") {
+			$link = $url;
+		}
+		else if (substr($link, 0 , 7) == "mailto:") {
+			$link = "[".$link."]";
+		}
+		else {
+			if (substr($link, 0 , 1) != "/") {
+				$link = $base_url."/".$link;
+			}
+			else{
+				$link = $base_url.$link;
+			}
+		}
+
+
+		if (substr($link, 0 , 7) != "http://"  &&  
+			substr($link, 0 , 8) != "https://" && 
+			substr($link, 0 , 1) != "[") {
+			if (substr($url , 0 , 8) == "https://") {
+				$link = "https://".$link;
+			}
+			else
+			{
+				$link = "http://".$link;
+			}
+		}
+
+		if (!in_array($link, $c)) {
+			array_push($c, $link);
+		}
 	}
 	
 }
 
 get_links($to_crawl);
+
+foreach ($c as $page) {
+	get_links($page);
+}
+
+foreach ($c as $page) {
+	echo $page."</br>";
+}
